10089fe763074163a2701addb821e178ed25e084
Chad Wagner <chawagne@gmail.com>
2017-02-22 22:04:28 -0700

    all done

--- main.js
+++ main.js
@@ -24,13 +24,63 @@ $("tbody tr").each(function(){
 //  console.log($(this).children("td:nth-child(3)").text());
 });
 
+//Question 5
+
+$(".btn-warning").on("click", function () {
+  $("tbody tr").each(function () {
+    if ($(this).children("td:nth-child(2)").text() < 10){
+         $(this).hide();
+    }
+   })
+})
+
+//Question 6
+
+$(".btn-success").on("click", function () {
+  $("tbody tr").each(function () {
+    var upper = $(this).children("td:nth-child(1)").text().toUpperCase();
+    $(this).children("td:nth-child(1)").text(upper);
+   })
+})
+
+//Question 7
+
+$(".btn-primary").on("click", function() {
+    $(".table-striped").toggle();
+});
+
+//Question 8
+$("#disableUs input").prop("disabled", true);
+$("#disableUs a").removeAttr("href");
+
+//Question 9
+$(".dropdown").change(function () {
+    $("<h3>" + $(this).find("option:selected").text() + "</h3>").insertAfter(".dropdown");
+});
 
 
+//Question 10
+$("#alert10").on("click",function () {
+  alert($("#alertList li").length);
+  counter();
+})
 
+//Question 11
+$("<input type='button' class='btn btn-secondary' value='add'/>").insertBefore("#alertList");
+$("<input type='button' class='btn btn-danger' value='remove'/>").insertBefore("#alertList");
 
+$(".btn-secondary").on("click", function() {
+    $("#alertList").append("<li>I'm new</li>");
+    counter();
+})
 
+$(".btn-danger").on("click", function() {
+    $("#alertList").children("li:last-child").remove();
+    counter();
+})
 
-
-
-
+//Question 12
+function counter() {
+    $("#displayList12").text($("#alertList li").length);
+}
 });
